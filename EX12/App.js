811c13331495f70bbffa672159fdb42354eacbad
B1329034 <jtkao.tw@gmail.com>
2026-05-13 15:49:32 +0800

    Create App.js

---
+++ EX12/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignInSide from './cgu_login'; 
+
+function App() {
+  return (
+    // 這裡不要加任何額外的 className 或 style，讓元件自己控制
+    <SignInSide />
+  );
+}
+
+export default App;
